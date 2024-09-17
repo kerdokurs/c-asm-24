9a577d936bb0ffe56db85965f51e7606ed192f37
Kerdo Kurs <kerdokurs@gmail.com>
2024-09-17 17:37:12 +0300

    [PRAX-3] mitu faili ja struktuurid

--- src/main.c
+++ src/main.c
@@ -11,47 +11,48 @@ int main(void)
   stack_push(-40);
 
   /* Lihtsalt eemaldame pealmise väärtuse (-40) ja ei salvesta seda muutujasse. */
-  // stack_pop();
+  stack_pop();
 
   /* Eemaldame pealmise väärtuse ja salvestame selle muutujasse, et saaksime seda ekraanil kuvada. */
-  // int a = stack_pop();
-  // printf("%d\n", a);
+  int a = stack_pop();
+  printf("%d\n", a);
 
   // /* Eemaldame veel ühe elemendi ja trükime selle välja sealjuures kasutades juba eelnevalt defineeritud muutujat a. */
-  // a = stack_pop();
-  // printf("%d\n", a);
+  a = stack_pop();
+  printf("%d\n", a);
 
-  // stack_push(1);
-  // stack_push(2);
-  // stack_push(3);
-  // stack_push(4);
-  // stack_print();
+  stack_push(1);
+  stack_push(2);
+  stack_push(3);
+  stack_push(4);
+  stack_print();
 
-  // int sisend = 0;
-  // do
-  // {
-  //   int unused __attribute__((unused));
-  //   unused = scanf();
-  //   if (scanf("%d", &sisend) != 1)
-  //   {
-  //     // fprintf(stderr, "ei tagastanud 1 numbrit\n");
-  //     printf("ei tagastanud 1 numbrit\n");
-  //     // return EXIT_SUCCESS;
-  //     return EXIT_FAILURE;
-  //   }
+  int sisend = 0;
+  do
+  {
+    // int unused __attribute__((unused));
+    // unused = scanf("%d", &sisend);
+    // tegelikult peaks tegema kontrolli (vt scanf dokumentatsiooni)
+    if (scanf("%d", &sisend) != 1)
+    {
+      // fprintf(stderr, "ei tagastanud 1 numbrit\n");
+      printf("ei tagastanud 1 numbrit\n");
+      // return EXIT_SUCCESS;
+      return EXIT_FAILURE;
+    }
 
-  //   if (sisend == 0)
-  //   {
-  //   }
-  //   else if (sisend == 1)
-  //   {
-  //   }
-  //   else if (sisend == 2)
-  //   {
-  //   }
-  //   else
-  //   {
-  //     printf("vale sisend\n");
-  //   }
-  // } while (true);
+    if (sisend == 0)
+    {
+    }
+    else if (sisend == 1)
+    {
+    }
+    else if (sisend == 2)
+    {
+    }
+    else
+    {
+      printf("vale sisend\n");
+    }
+  } while (true);
 }
