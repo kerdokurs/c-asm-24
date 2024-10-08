800ffb4c47358a5ffb1c4a4790f0c5e0e334a03f
Kerdo Kurs <kerdokurs@gmail.com>
2024-10-08 17:46:56 +0300

    [PRAX-5,6] BF interpretaator

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
 CFLAGS := -std=c99 -g -Og -Werror -Wall -Wpedantic
 
 EXE := lib/main
+EXE_BT2C := lib/bt2c
 
 OBJS := obj/bfi.o obj/stack.o obj/mem.o
+OBJS_BT2C := obj/bt2c.o
 
 $(EXE): $(OBJS) lib
 	$(CC) -Iinc/ $(CFLAGS) -o $(EXE) $(OBJS)
+
+$(EXE_BT2C): $(OBJS_BT2C) lib
+	$(CC) -Iinc/ $(CFLAGS) -o $(EXE_BT2C) $(OBJS_BT2C)
 
 obj/%.o: src/%.c obj
 	$(CC) -c -Iinc/ $(CFLAGS) -o $@ $<
@@ -19,6 +24,10 @@ lib:
 .PHONY: run
 run: $(EXE)
 	./$(EXE)
+
+.PHONY: bt2c
+bt2c: $(EXE_BT2C)
+	./$(EXE_BT2C)
 
 .PHONY: clean
 clean:
--- src/bfi.c
+++ src/bfi.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 #include "mem.h"
+#include "stack.h"
+
+#ifdef DEBUG
+  #define PRINT_PARAMS "'%c' (kood = %d)\n", c, c
+#else
+  #define PRINT_PARAMS "%c", c
+#endif
 
 /* Selles enumeratsioonis on toodud kõik Brainfuck'i keele sümbolid. */
 enum instructions_e {
@@ -51,6 +58,41 @@ void interpret(const char *program) {
             case BF_DEBUG:
                 mem_printDebug();
                 break;
+            case BF_READ: {
+                char c = getc(stdin);
+
+                // Võimalik, et Windows'i süsteemidel on see vajalik
+                /* while (c == '\n' || c == '\r') {
+                    c = getc(stdin);
+                } */
+                mem_set(c);
+                break;
+            }
+            case BF_PRINT: {
+                char c = mem_get();
+                printf(PRINT_PARAMS);
+                break;
+            }
+            case BF_START_LOOP: {
+                if (mem_get() == 0) {
+                    int loop = 1;
+                    while (loop > 0) {
+                        i++;
+                        if (program[i] == BF_START_LOOP) {
+                            loop++;
+                        } else if (program[i] == BF_END_LOOP) {
+                            loop--;
+                        }
+                    }
+                } else {
+                    stack_push(i);
+                }
+                break;
+            }
+            case BF_END_LOOP: {
+                i = stack_pop() - 1;
+                break;
+            }
             default:
                 break;
                 /* Ignoreerime sümboleid, mida me ei tunne. */
@@ -60,9 +102,23 @@ void interpret(const char *program) {
     }
 }
 
-int main(void) {
-    interpret("++++>>++<<#");
-    /* Eeldatav väljund:
-       index: 0 mem[0 .. 9]: 4 0 2 0 0 0 0 0 0 0
+int main(int argc, char **argv, char **envp) {
+    if (argc != 2) {
+        fprintf(stderr, "Programmil peab olema üks parameeter, milleks on BF programm!\n");
+        return 1;
+    }
+
+    interpret(argv[1]);
+    return 0;
+    /*
+    printf("Argumendid:\n");
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+
+    printf("Keskkonnamuutujad:\n");
+    for (int i = 0; envp[i] != NULL; i++) {
+        printf("envp[%d] = %s\n", i, envp[i]);
+    }
     */
 }
